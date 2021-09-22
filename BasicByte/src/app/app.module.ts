7c4cd8329149d3f93e5b4408cbba405dca294fde
Mortos98 <luke.sturgiss@gmail.com>
2021-09-22 18:05:00 +1000

    Variable creation

--- BasicByte/src/app/app.module.ts
+++ BasicByte/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadderComponent } from './headder/headder.component';
+import { DarkToggleComponent } from './dark-toggle/dark-toggle.component';
+import { MenuNavigationComponent } from './menu-navigation/menu-navigation.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadderComponent,
+    DarkToggleComponent,
+    MenuNavigationComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
