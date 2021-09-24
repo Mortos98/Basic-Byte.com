6897eb6a302299ba64365c3f00bdcd7c4d2c6509
BasicByte <31377905+Mortos98@users.noreply.github.com>
2021-09-25 08:19:21 +1000

    transfer laptop

--- BasicByte/src/app/search-competencies/search-competencies.component.ts
+++ BasicByte/src/app/search-competencies/search-competencies.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faAngular, faJsSquare, faCss3, faHtml5, faJava, faPython, faNode
- } from '@fortawesome/free-brands-svg-icons';
+import { faAngular, faJsSquare, faCss3, faHtml5, faJava, faPython,faDocker,faVuejs, faReact, faGit,faJira, faLinux, faRaspberryPi, faSass, faWindows, faYarn, faNodeJs, faNpm} from '@fortawesome/free-brands-svg-icons';
+import { faNetworkWired, faShieldAlt, faDatabase, faDesktop } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-competencies',
@@ -9,6 +9,28 @@ import { faAngular, faJsSquare, faCss3, faHtml5, faJava, faPython, faNode
 })
 export class SearchCompetenciesComponent implements OnInit {
   faAngular=faAngular;
+  faJsSquare = faJsSquare;
+  faCss3 = faCss3;
+  faHtml5 = faHtml5;
+  faJava = faJava;
+  faPython = faPython;
+  faNodejs = faNodeJs;
+  faNpm = faNpm;
+  faLinux = faLinux;
+  faDocker = faDocker; 
+  faVuejs = faVuejs;
+  faReact = faReact;
+  faGit = faGit;
+  faJira = faJira;
+  faRaspberryPi = faRaspberryPi;
+  faSass = faSass;
+  faWindows = faWindows;
+  faYarn = faYarn;
+  faNetworkWired = faNetworkWired;
+  faShieldAlt = faShieldAlt;
+  faDatabase = faDatabase;
+  faDesktop = faDesktop;
+
   constructor() { }
 
   ngOnInit(): void {
